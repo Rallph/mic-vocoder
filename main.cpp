ff3055a760513058e3d4cd4a3e6042793fc393e6
Ralph Nahra <ralph-nahra@uiowa.edu>
2022-10-15 16:13:06 -0500

    Get name of output device

--- main.cpp
+++ main.cpp
@@ -9,27 +9,39 @@ int main() {
     const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
     const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
 
+    // create multimedia device enumerator
     IMMDeviceEnumerator* pEnumerator = nullptr;
-
     HRESULT hr = CoCreateInstance(
             CLSID_MMDeviceEnumerator, NULL,
             CLSCTX_ALL, IID_IMMDeviceEnumerator,
             (void**)&pEnumerator);
 
+    // get default audio capture device as endpoint
     IMMDevice* ppEndpoint = nullptr;
-
     hr = pEnumerator->GetDefaultAudioEndpoint(eCapture, eCommunications, &ppEndpoint);
 
+    // get properties of the audio endpoint
     IPropertyStore* propertyStore = nullptr;
-
     hr = ppEndpoint->OpenPropertyStore(STGM_READ, &propertyStore);
-
     PROPVARIANT varName;
     PropVariantInit(&varName);
-
+    // get the human-readable name of the device associated with the endpoint
     propertyStore->GetValue(PKEY_Device_FriendlyName, &varName);
+    printf("Input Audio endpoint name: %S\n", varName.pwszVal);
 
-    printf("Audio endpoint name: %S\n", varName.pwszVal);
+    IMMDevice* ppEndpoint2 = nullptr;
+    hr = pEnumerator->GetDefaultAudioEndpoint(eRender, eMultimedia, &ppEndpoint2);
+
+    // get properties of the audio endpoint
+    IPropertyStore* propertyStore2 = nullptr;
+    hr = ppEndpoint2->OpenPropertyStore(STGM_READ, &propertyStore2);
+    PROPVARIANT varName2;
+    PropVariantInit(&varName2);
+    // get the human-readable name of the device associated with the endpoint
+    propertyStore2->GetValue(PKEY_Device_FriendlyName, &varName2);
+    printf("Output Audio endpoint name: %S\n", varName2.pwszVal);
+
+
 
 
     CoUninitialize();

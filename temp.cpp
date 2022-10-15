5ff422f7eaf3729e30ba886845169379cd718d9a
John Hofmann <johnphofmann33@gmail.com>
2022-10-15 12:55:45 -0500

    update

--- temp.cpp
+++ temp.cpp
@@ -19,3 +19,26 @@ HRESULT EnumAudioEndpoints(
 
 
 
+const IID IID_IAudioClient  __uuidof(IAudioCaptureClient)
+//The Activate method creates a COM object with the specified interface.
+ iid = GetDefaultAudioEndpoint('below') //The interface identifier.
+ dwClsCtx = CLSCTX_ALL
+ pActivationParams = NULL
+
+         HRESULT
+         Activate(
+             [in] REFIID iid,
+             [in] DWORD dwClsCtx,
+             [in] PROPVARIANT *pActivationParams,
+             [out] void **ppInterface);
+             //If the method succeeds, it returns S_OK
+
+ // obtain a counted reference to the interface (input)
+ dataFlow = eCapture
+     role = eCommunications
+
+         HRESULT
+         GetDefaultAudioEndpoint(
+             [in] EDataFlow dataFlow,
+             [in] ERole role,
+             [out] IMMDevice **ppEndpoint);

83f33b75eff3f5cc46cea2ef885a315734794e0c
John Hofmann <johnphofmann33@gmail.com>
2022-10-15 15:00:03 -0500

    update 4

--- temp.cpp
+++ temp.cpp
@@ -2,7 +2,7 @@
 
 
 
-#hr = CoCreateInsatanceEx(NULL);
+//hr = CoCreateInsatanceEx(NULL);
 
 
 
@@ -70,3 +70,28 @@ DWORD  waveInMessage(
    DWORD_PTR dwParam1,
    DWORD_PTR dwParam2
 );
+
+
+----------------------------
+
+
+    CoCreateInsatanceEx???
+• CoCreateInstance function to create a device enumerator.  is an object with an IMMDeviceEnumerator interface
+•  IMMDeviceEnumerator::EnumAudioEndpoints method to create a collection of endpoint objects.
+	◦ (list of endpoint object)
+	◦ IMMDeviceEnumerator::GetDefaultAudioEndpoint
+		‣ IMMDevice **ppEndpoint
+A device collection is an object with an IMMDeviceCollection interface. Each item in a device collection is an endpoint object with at least the following two interfaces:
+
+   -  An IMMDevice interface. A client obtains a reference to the IMMDevice interface of an endpoint object in a device collection by calling the IMMDeviceCollection::Item method.
+   -  An IMMEndpoint interface. A client obtains a reference to the IMMEndpoint interface of an endpoint object by calling the IMMDevice::QueryInterface method.
+
+• After selecting a suitable device, the client can call the IMMDevice::Activate method to activate the device-specific interfaces in WASAPI 
+	◦ REFIID      iid = IID_IAudioEndpointVolume
+		‣ const IID IID_IAudioClient  __uuidof(IAudioCaptureClient)
+	◦ DWORD dwClsCtx = CLSCTX_ALL, 
+	◦ PROPVARIANT *pActivationParams = 
+
+
+IAudioEndpointVolume::SetChannelVolumeLevel
+345 dev on cloud,
